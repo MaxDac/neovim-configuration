b50337aa9dbdef41feac3dca9711a974be26e060
Massimiliano D'Acunzo <massimiliano.dacunzo@hotmail.com>
2022-07-18 17:25:48 +0100

    Specified languages to manage in neovim

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
+  ensure_installed = { "elixir", "lua", "javascript", "html", "css" },
   sync_install = false,
   ignore_install = { },
   highlight = {

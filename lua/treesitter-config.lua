cc250a0f750a2ffdf50ddeaf343854bf688ac4cc
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-09-20 08:55:16 +0100

    Added eex and heex template syntax highlighting

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "elixir", "lua", "javascript", "html", "css", "surface" },
+  ensure_installed = { "elixir", "lua", "javascript", "html", "css", "eex", "heex", "surface" },
   sync_install = false,
   ignore_install = { },
   highlight = {

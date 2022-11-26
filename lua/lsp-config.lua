924208e9715b9bae8a00056ecaec7cd848066614
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-11-26 12:02:45 +0000

    Updated to NeoVim 0.8.1

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -32,7 +32,7 @@ local rust_on_attach = function(client, bufnr)
   require('completion').on_attach(client)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- lspconfig.sumneko_lua.setup {
 --   settings = {

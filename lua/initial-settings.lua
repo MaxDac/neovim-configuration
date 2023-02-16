0c1a7053da73ea452d3093016c4bbf2a3b631f7e
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-02-16 13:40:36 +0000

    Added quick buffer switch shortcuts

--- lua/initial-settings.lua
+++ lua/initial-settings.lua
@@ -10,4 +10,6 @@ vim.o.termguicolors = true
 
 -- Trying to associate the command ':lua vim.lsp.buf.definition()' to a key shortcut 
 vim.api.nvim_set_keymap('n', '<C-b>', ':lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-n>', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-N>', ':bprevious<CR>', { noremap = true, silent = true })
 

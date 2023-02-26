f05c7a3004e46dbda8af9d09b8058c61277f2460
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-02-26 12:02:39 +0000

    Adding color palettes

--- lua/initial-commands.lua
+++ lua/initial-commands.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd('VimEnter', { command = 'NvimTreeToggle' })
 -- vim.api.nvim_create_autocmd('VimEnter', { command = 'NERDTree' })
 
+vim.cmd [[hi ColorColumn ctermbg=DarkGray guibg=DarkGray]]
 vim.cmd [[silent! colorscheme halcyon]]
 vim.cmd [[set colorcolumn=120]]
-vim.cmd [[hi ColorColumn ctermbg=DarkGray guibg=DarkGray]]
 

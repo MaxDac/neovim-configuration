4181214e0284ae2f603817e9a1b561f47e783f40
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-07-22 14:48:47 +0100

    Changed vertical line color

--- lua/initial-commands.lua
+++ lua/initial-commands.lua
@@ -4,4 +4,5 @@ vim.api.nvim_create_autocmd('VimEnter', { command = 'NvimTreeToggle' })
 vim.cmd [[hi ColorColumn ctermbg=DarkGray guibg=DarkGray]]
 vim.cmd [[silent! colorscheme halcyon]]
 vim.cmd [[set colorcolumn=120]]
+vim.cmd [[highlight ColorColumn ctermbg=darkgrey guibg=darkgrey]]
 

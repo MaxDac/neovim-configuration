f2e613282210b3dfdf6756b11131fffeaa45fd54
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-03 15:31:17 +0100

    Changing Copilot shortcut for tmux compatibility

--- lua/8-copilot.lua
+++ lua/8-copilot.lua
@@ -1,4 +1,4 @@
 -- Adding custom mapping for Copilot than TAB
 vim.keymap.set('i', '<M-.>', '<Plug>(copilot-next)', { noremap = false })
 vim.keymap.set('i', '<M-,>', '<Plug>(copilot-previous)', { noremap = false })
-vim.keymap.set('i', '<C-b>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
+vim.keymap.set('i', '<C-/>', "copilot#Accept('<CR>')", { expr = true, silent = true })
